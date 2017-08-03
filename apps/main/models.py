58a6eef9a491c77e619e4f65d27e262b87ea473a
Rob deCarvalho <robdmc@gmail.com>
2017-08-03 19:37:45 -0400

    Changed modeling to match new ui

--- apps/main/models.py
+++ apps/main/models.py
@@ -17,33 +17,16 @@ class Experiment(models.Model):
 
 
 class Configuration(models.Model):
-    label = models.CharField(max_length=CHAR_LENGTH, null=True, blank=True)
-    experiment = models.ForeignKey('main.Experiment')
-    last_modified= models.DateField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.label:
-            self.label = str(self)
-        super(Configuration, self).save(*args, **kwargs)
-
-    def __str__(self):
-        if self.label:
-            return self.label
-        else:
-            return 'Config_{:04d}_{}'.format(self.id, self.last_modified)
-
-
-class Description(models.Model):
-    notes = models.ForeignKey('main.Configuration')
     name = models.CharField(max_length=CHAR_LENGTH)
-    value = models.TextField()
+    experiment = models.ForeignKey('main.Experiment')
+    notes = models.TextField()
 
     def __str__(self):
         return self.name
 
 
 class Parameter(models.Model):
-    notes = models.ForeignKey('main.Configuration')
+    configuration = models.ForeignKey('main.Configuration')
     name = models.CharField(max_length=CHAR_LENGTH)
     value = models.CharField(max_length=CHAR_LENGTH)
     type = models.CharField(max_length=CHAR_LENGTH, choices=TYPE_CHOICES)
@@ -52,53 +35,12 @@ class Parameter(models.Model):
         return self.name
 
 
-class EntryInlineAdmin(admin.TabularInline):
-    model = Parameter
 
+class TabularInlineBase(admin.TabularInline):
     def has_change_permission(self, request, obj=None):
         return True
 
     def has_add_permission(self, request):
-        return True
-
-    def has_delete_permission(self, request, obj=None):
-        return True
-
-    def get_extra(self, request, obj=None, **kwargs):
-        return 0
-
-
-class DescriptionInlineAdmin(admin.TabularInline):
-    model = Description
-
-    def has_change_permission(self, request, obj=None):
-        return True
-
-    def has_add_permission(self, request):
-        return True
-
-    def has_delete_permission(self, request, obj=None):
-        return True
-
-    def get_extra(self, request, obj=None, **kwargs):
-        return 0
-
-
-@admin.register(Configuration)
-class ConfigurationAdmin(admin.ModelAdmin):
-    inlines = [
-        EntryInlineAdmin,
-        DescriptionInlineAdmin
-
-    ]
-
-class ConfigurationInlineAdmin(admin.TabularInline):
-    model = Configuration
-
-    def has_add_permission(self, request):
-        return True
-
-    def has_change_permission(self, request, obj=None):
         return True
 
     def has_delete_permission(self, request, obj=None):
@@ -109,6 +51,20 @@ class ConfigurationInlineAdmin(admin.TabularInline):
 
     def show_change_link(self):
         return True
+
+class ParameterInlineAdmin(TabularInlineBase):
+    model = Parameter
+
+
+class ConfigurationInlineAdmin(TabularInlineBase):
+    model = Configuration
+
+
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    inlines = [
+        ParameterInlineAdmin,
+    ]
 
 
 @admin.register(Experiment)
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+#from django.views.generic import ListView
 from django.views import View
 from django.shortcuts import render
-from apps.main.models import Experiment, Configuration, Parameter, Description
+#from apps.main.models import Experiment, Configuration, Parameter, Description
 from apps.main.models import Experiment
 
 class ExperimentList(View):
