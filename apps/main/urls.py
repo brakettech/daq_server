fc16001c6857cc4bfda95cd33c7b9a7c23901f99
Rob deCarvalho <robdmc@gmail.com>
2017-08-20 22:14:54 -0400

    working on nav

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -20,19 +20,20 @@ from rest_framework import routers
 #router.register(r'parameter', views.ParamDetail)
 
 urlpatterns = [
-    #url(r'^experiment/(?P<experiment_id>\d+)*/(?P<configuration_id>\d+)*$', ExperimentView.as_view()),
-    url(r'^experiment/?$', ExperimentListView.as_view()),
-    url(r'^experiment/(?P<experiment_id>\d+)/?$', ConfigListView.as_view()),
+    url(r'^experiment/?$', ExperimentListView.as_view(), name='experiments'),
+    url(r'^experiment/(?P<experiment_id>\d+)/?$', ConfigListView.as_view(), name='experiment'),
+    url(r'^new_experiment/?$', NewExperimentView.as_view()),
+    url(r'^delete_experiment/(?P<pk>\d+)/?$', DeleteExperimentView.as_view()),
+
+    url(r'^config/(?P<config_id>\d+)/?$', ParamListView.as_view(), name='config'),
+
     url(r'^clone_experiment/(?P<experiment_id>\d+)/?$', CloneExperimentView.as_view()),
     url(r'^clone_config/(?P<config_id>\d+)/?$', CloneConfigView.as_view()),
-    url(r'^new_experiment/?$', NewExperimentView.as_view()),
     url(r'^new_config/(?P<experiment_id>\d+)/?$', NewConfigView.as_view()),
     url(r'^new_param/(?P<config_id>\d+)/?$', NewParamView.as_view()),
     url(r'^change_param/params(?P<param_id>\d+)/?$', ChangeParamView.as_view()),
     url(r'^delete_param/(?P<param_id>\d+)/?$', DeleteParamView.as_view()),
-    url(r'^delete_experiment/(?P<pk>\d+)/?$', DeleteExperimentView.as_view()),
     url(r'^delete_config/(?P<pk>\d+)/?$', DeleteConfigView.as_view()),
-    url(r'^config/(?P<config_id>\d+)/?$', ParamListView.as_view()),
     # url(r'^experiment/(?P<experiment_id>\d+)/(?P<configuration_id>\d+)/?$', ExperimentView.as_view()),
     # url(r'parameters/(?P<configuration_id>\d+)*$', views.ParamList.as_view()),
     # #url(r'parameters', views.ParamList.as_view()),
