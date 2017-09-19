bcbe2f5d0259449b8a30bc1e3281279a5ca4a2f5
Rob deCarvalho <robdmc@gmail.com>
2017-09-18 20:07:27 -0400

    removed print statement

--- daq/settings.py
+++ daq/settings.py
@@ -14,10 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('*'*80)
-print(BASE_DIR)
-print('*'*80)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
